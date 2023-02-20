6d0bbd1328ba3da80ffea6b88a49fdbed29cbbd7
carlos <carloslin@phalanxbiotech.com>
2023-02-20 11:56:08 +0800

    map Card components

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "@/styles/Home.module.css";
-import Banner from "@/components/Banner";
+import Banner from "@/components/banner";
+import Card from "@/components/card";
+import coffeeStores from "../data/coffee-stores.json";
 export default function Home() {
   const handleOnBannerBtnClick = () => {
     console.log("hi banner component");
@@ -19,7 +21,23 @@ export default function Home() {
           handleOnClick={handleOnBannerBtnClick}
         />
         <div className={styles.heroImage}>
-          <Image src="/static/hero-image.png" width={700} height={400} />
+          <Image
+            src="/static/hero-image.png"
+            width={700}
+            height={400}
+            alt="coffee-shop-people"
+          />
+        </div>
+        <div className={styles.cardLayout}>
+          {coffeeStores.map((coffeeStore) => (
+            <Card
+              key={coffeeStore.id}
+              name={coffeeStore.name}
+              imgUrl={coffeeStore.imgUrl}
+              href={`/coffee-store/${coffeeStore.id}`}
+              className={styles.card}
+            />
+          ))}
         </div>
       </main>
     </div>
